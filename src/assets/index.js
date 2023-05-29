c9a6efce5b59ec142affbf25c37f11511d26b935
Rajeev <rajeevkr.developer@gmail.com>
2023-05-29 19:27:14 +0530

    changes

---
+++ src/assets/index.js
@@ -0,0 +1,98 @@
+import bwmap from "./backgrounds/code.jpg";
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/code.jpg";
+import worldmap from "./backgrounds/code.jpg";
+
+import logo from "./logo/logo-black.png";
+// import logotext from "./logo/logo-text-black.png";
+
+import raj from "./personal/raj.png";
+import backend from "./icons/backend.png";
+import ux from "./icons/ux.png";
+import frontend from "./icons/frontend.png";
+import prototyping from "./icons/prototyping.png";
+import github from "./icons/github.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
+
+import css from "./tech/css.png";
+import next from "./tech/next.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import graphql from "./tech/graphql.png";
+import postgresql from "./tech/postgresql.png";
+import python from "./tech/python.png";
+
+import coverhunt from "./company/coverhunt.png";
+import dcc from "./company/dcc.png";
+import kelhel from "./company/kelhel.png";
+import microverse from "./company/microverse.png";
+
+import hfio from "./projects/hfio.png";
+import hflanding from "./projects/hflanding.png";
+import emailauth from "./projects/emailauth.png";
+import movie from "./projects/movie-metro.png";
+import nyeusi from "./projects/nyeusi.png";
+import space from "./projects/space-hub.png";
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  // logotext,
+  raj,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  next,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  nodejs,
+  postgresql,
+  python,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  hfio,
+  hflanding,
+  emailauth,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+};
